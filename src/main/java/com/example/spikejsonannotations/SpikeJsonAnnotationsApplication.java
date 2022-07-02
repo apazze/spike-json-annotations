e93513c6c991a89d50a49e48aeaacd91b58282a8
apazze <apazze@users.noreply.github.com>
2022-07-02 20:26:49 -0300

    Commit inicial

---
+++ src/main/java/com/example/spikejsonannotations/SpikeJsonAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spikejsonannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeJsonAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikeJsonAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spikejsonannotations/SpikeJsonAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spikejsonannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikeJsonAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
